6aa43824de57df7cdeb90d5d28fb6323310e81ba
pk <p.kaske@gmail.com>
2020-07-03 00:48:05 +0200

    Add config

--- server/config/production.js
+++ server/config/production.js
@@ -18,6 +18,16 @@ module.exports = {
         startTime: new Date('2019-08-01T00:00:00.000Z'),
         apiKey: process.env.XBTUSD_LRAIC_KEY,
         apiSecret: process.env.XBTUSD_LRAIC_SECRET,
+      },
+      {
+        name: 'ETH_PERP_LRAIC',
+        exchange: 'FTX',
+        symbol: 'ETH-PERP',
+        startTime: new Date('2019-10-19T00:00:00.000Z'),
+        apiKey: process.env.ETHPERP_LRAIC_KEY,
+        apiSecret: process.env.ETHPERP_LRAIC_SECRET,
+        subAccount: process.env.ETHPERP_LRAIC_SUBACCOUNT,
+        balance: 100
       }
     ]
   },
